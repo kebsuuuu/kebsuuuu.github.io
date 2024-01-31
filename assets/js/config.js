32daee9339cea1cf922034d2a48c545133bce753
kebsuuuu <158283778+kebsuuuu@users.noreply.github.com>
2024-01-31 14:14:21 +0100

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,35 @@
+const BADGES = [
+  {
+    name: "Active Developer",
+    image: "assets/badges/Active_Developer.svg",
+  },
+  {
+    name: "Subscriber since Oct 2, 2023",
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Oct 6, 2023",
+    image: "assets/badges/boost3month.svg",
+  },
+];
+
+const USER_ID = "902988153701404742"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/files/bg.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
