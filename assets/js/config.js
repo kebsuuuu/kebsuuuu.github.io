7e3c3ffbf682ae5289204e3488c94d55301b098b
kebsuuuu <158283778+kebsuuuu@users.noreply.github.com>
2024-02-01 18:11:14 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,14 +3,6 @@ const BADGES = [
     name: "Active Developer",
     image: "assets/badges/Active_Developer.svg",
   },
-  {
-    name: "Subscriber since Oct 2, 2023",
-    image: "assets/badges/nitro.svg",
-  },
-  {
-    name: "Server boosting Oct 6, 2023",
-    image: "assets/badges/boost3month.svg",
-  },
 ];
 
 const USER_ID = "902988153701404742"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
